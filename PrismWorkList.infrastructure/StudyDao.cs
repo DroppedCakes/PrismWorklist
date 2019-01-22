496ffd06fe82eec079155ba7d88a60b16c774919
fujioka <fujioka@ustec.co.jp>
2019-01-22 12:35:29 +0900

    [Fix]変更漏れ分

--- PrismWorkList.infrastructure/StudyDao.cs
+++ PrismWorkList.infrastructure/StudyDao.cs
@@ -5,31 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using PrismWorkList.Infrastructure.Models;
 using Dapper.FastCrud;
+using System.Data;
 
 namespace PrismWorkList.Infrastructure
 {
     public class StudyDao
     {
-        private readonly ITransactionContext _transactionContext;
+        private readonly IDbConnection _dbConnection;
 
-        public StudyDao(ITransactionContext transactionContext)
+        public StudyDao(IDbConnection dbConnection)
         {
-            _transactionContext = transactionContext;
+            this._dbConnection = dbConnection;
         }
 
         public virtual ExaminationOrder FindByOrderNumber(string orderNumber)
         {
-            return _transactionContext.Connection.Find<ExaminationOrder>(statement =>
+            return _dbConnection.Find<ExaminationOrder>(statement =>
             statement
             .Where($"{nameof(ExaminationOrder.OrderNumber)}=''{orderNumber}")
             ).SingleOrDefault();
         }
 
         public virtual void Update(ExaminationOrder examinationOrder)
-            => _transactionContext.Connection.Update(examinationOrder);
+            =>_dbConnection.Update(examinationOrder);
 
         public virtual void Insert(ExaminationOrder examinationOrder)
-            => _transactionContext.Connection.Insert(examinationOrder);
+            => _dbConnection.Insert(examinationOrder);
 
     }
 }
