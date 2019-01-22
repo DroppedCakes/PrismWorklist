7431f1a7465995a9234eb5e5685a923176739ca5
fujioka <fujioka@ustec.co.jp>
2019-01-22 13:51:48 +0900

    [Fix]OrderPatientViewDao実装

---
+++ PrismWorkList.Infrastructure/OrderPatientViewDao.cs
@@ -0,0 +1,31 @@
+﻿using Dapper.FastCrud;
+using PrismWorkList.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismWorkList.Infrastructure
+{
+    class OrderPatientViewDao
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public OrderPatientViewDao(IDbConnection dbConnection)
+        {
+            this._dbConnection = dbConnection;
+        }
+
+        public IEnumerable<OrderPatientView> FindByOrderNumber(string orderNumber)
+            => _dbConnection.Find<OrderPatientView>();
+
+        public virtual void Update(OrderPatientView examinationOrder)
+            => _dbConnection.Update(examinationOrder);
+
+        public virtual void Insert(OrderPatientView examinationOrder)
+            => _dbConnection.Insert(examinationOrder);
+
+    }
+}
