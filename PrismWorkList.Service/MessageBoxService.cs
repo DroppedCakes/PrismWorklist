ffc8bc68df41428ba5fb673eed6aa9868b02ed85
fujioka <fujioka@ustec.co.jp>
2019-04-02 10:34:52 +0900

    MessageBoxServiceを実装

---
+++ PrismWorkList.Service/MessageBoxService.cs
@@ -0,0 +1,60 @@
+﻿using Prism.Interactivity.InteractionRequest;
+using System.Windows;
+
+namespace PrismWorkList.Service
+{
+    public class MessageBoxService : IMessageBoxService
+    {
+        public InteractionRequest<INotification> MessageBoxRequest { get; }
+
+        public MessageBoxResult ShoowConfirmMessage(string message)
+            => this.ShowConfirmMessage(message, "確認");
+
+        public MessageBoxResult ShowConfirmMessage(string message, string title)
+        {
+            var confirm = new Confirmation()
+            {
+                Content = message,
+                Title = title
+            };
+
+            var messageResult = MessageBoxResult.Cancel;
+            this.MessageBoxRequest.Raise(confirm, r =>
+             {
+                 messageResult = (r as IConfirmation).Confirmed ? MessageBoxResult.OK : MessageBoxResult.Cancel;
+             });
+
+            return messageResult;
+        }
+
+        public MessageBoxResult ShowConfirmMessage(string message)
+               => this.ShowConfirmMessage(message, "確認");
+
+        /// <summary>情報メッセージボックスを表示します。</summary>
+        /// <param name="message">メッセージボックスに表示する内容を表す文字列。</param>
+        /// <param name="title">メッセージボックスのタイトルを表す文字列。</param>
+        public void ShowInformationMessage(string message, string title)
+        {
+            var notify = new Notification()
+            {
+                Content = message,
+                Title = title
+            };
+
+            this.MessageBoxRequest.Raise(notify);
+        }
+
+        public void ShowInformationMessage(string message)
+        {
+            this.ShowInformationMessage(message, "情報");
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MessageBoxService()
+        {
+            this.MessageBoxRequest = new InteractionRequest<INotification>();
+        }
+    }
+}
